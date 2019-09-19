b3e9f71c6e9b0f6560524c9b90ad5b16669e1404
Nicolas Ormeno <ni.ormeno@gmail.com>
2019-09-19 13:29:24 -0300

    Add find prime

---
+++ src/prime/prime.ts
@@ -0,0 +1,72 @@
+class Prime {
+  validNumber = (originalNum: number, recursive: boolean = false) => {
+    let result: boolean = true;
+    
+    result = (recursive)
+      ? this.recursive(originalNum) : this.validPrime(originalNum); 
+    
+    return result;
+  }
+
+  private formatNumber = (num: number, maxPosition: number) => {
+    const aNumber: Array<string> | number = Array.from(num.toString());
+  
+    // Remove number by position
+    for(let i = aNumber.length; i >= 0; i--) {
+      if (i > maxPosition) {
+        aNumber.splice(i, 1);
+      }
+    }
+  
+    return Number(aNumber.join(''));
+  }
+
+  private validPrime = (num: number) => {
+    let isPrime: boolean = true;
+    let invalidBy: Array<number> = [];
+
+    for (let i = 2; i < num; i++) {
+      if (num % i === 0) {
+        isPrime = false;
+        invalidBy.push(i);
+      }
+    }
+
+    return isPrime;
+  }
+
+  private recursive = (originalNum: number) => {
+    let result: boolean = true;
+
+    for (let i: number = originalNum.toString().length -1; i >= 0; i--) {
+      let num: number = this.formatNumber(originalNum, i);
+      const isPrime: boolean = this.validPrime(num);
+      
+      if (!isPrime) {
+        result = false;
+      }
+    }
+
+    return result;
+  }
+}
+
+const recursiveNumbers: Array<number> = [3753, 73, 3793];
+const simpleNumbers: Array<number> = [7, 103, 51];
+const prime: Prime = new Prime();
+
+recursiveNumbers.forEach((value) => {
+  const valid = prime.validNumber(value, true);
+  
+  (valid)
+    ? console.log(`${value} is prime (recursive)`)
+    : console.log(`${value} is not prime (recursive)`);
+});
+
+simpleNumbers.forEach((value) => {
+  const valid = prime.validNumber(value, true);
+  
+  (valid)
+    ? console.log(`${value} is prime`)
+    : console.log(`${value} is not prime`);
+});
